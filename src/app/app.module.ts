61073b2fc7ff0190878bbe26141e315b7e5d3922
Aisa <aisajemila@gmail.com>
2019-03-28 16:18:56 +0300

    Created a giphy component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GiphComponent } from './giph/giph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GiphComponent
   ],
   imports: [
     BrowserModule,
